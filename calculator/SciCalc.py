58313b63f6ee4e1f028bff559ee433d978a57216
ishmeet <theonlyrealemailid@gmail.com>
2020-01-22 12:51:11 +0530

    fixed bugs

--- calculator/SciCalc.py
+++ calculator/SciCalc.py
@@ -1,33 +1,65 @@
 import math
+import calc
 
-def SciCal():
+def print_menu():
+
+    print('*******')
+    print("Scientific Calculator")
+    print('*******')
+
     print('[1] Inverse')
     print('[2] Square')
     print('[3] Cube')
     print('[4] Root')
     print('[5] Log base 10')
     print('[6] Log base 2')
+    print('[x] Exit to Simple calculator')
 
-    operator = input('Enter your option\n')
+def clear():
+    print('\n' * 50)
+    # 50 lines distance to keep cli clean looking
 
 
+def SciCal():
 
-    oprand = float(input("Enter input\n"))
+    loop = True
+    while(loop):
 
-
-    if(operator == '1'):
-        print("Result : ",1/oprand)
-    elif(operator == '2'):
-        print("Result : ",oprand**2)
-    elif(operator == '3'):
-        print("Result : ",oprand**3)
-    elif(operator == '4'):
-        print("Result : ",oprand**0.5)
-    else:
-        print(" Invalid Input....Try again")
-        SciCal
+        print_menu()
         
+        operator = input('Enter your option\n')
+        if(operator == 'x'):
+            loop = False
+            calc.main()
+            continue
+        oprand = float(input("Enter input\n"))
+
+        if(operator == '1'):
+            print("Result : ",1/oprand)
+            input()
+            clear()
+        elif(operator == '2'):
+            print("Result : ",oprand**2)
+            input()
+            clear()
+
+        elif(operator == '3'):
+            print("Result : ",oprand**3)
+            input()
+            clear()
+        elif(operator == '4'):
+            print("Result : ",oprand**0.5)
+            input()
+            clear()
+        else:
+            print(" Invalid Input....Try again")
+            input()
+            clear()
+        
+        
+def main():
     
+    SciCal()
 
 if __name__ == "__main__":
     SciCal()      
--- calculator/calc.py
+++ calculator/calc.py
@@ -17,54 +17,60 @@ def clear():
     print('\n' * 50)
     # 50 lines distance to keep cli clean looking
 
-opc = ''
-error = True
 
-while(opc != 'x' ):
-    clear()
-    print_menu()
-    opc = input("Please select an option: ")
+def simpleCalc():
+    opc = ''
+    error = True
+    while(opc != 'x' ):
+        clear()
+        print_menu()
+        opc = input("Please select an option: ")
 
-    if(opc == '1' or opc == '2' or opc == '3' or opc == '4'):
+        if(opc == '1' or opc == '2' or opc == '3' or opc == '4'):
 
-        while(error):
-            try:
-                num1 = float(input("Input your first number "))
-                error = False
-            except:
-                error = True
-                print("That wasnt a valid number, try again\n")
-        error = True
-        while(error):
-            try:
-                num2 = float(input("Input your second number "))
-                error = False
-            except:
-                error = True
-                print("That wasnt a valid number, try again\n")
+            while(error):
+                try:
+                    num1 = float(input("Input your first number "))
+                    error = False
+                except:
+                    error = True
+                    print("That wasnt a valid number, try again\n")
+            error = True
+            while(error):
+                try:
+                    num2 = float(input("Input your second number "))
+                    error = False
+                except:
+                    error = True
+                    print("That wasnt a valid number, try again\n")
 
-            
-        if(opc == '4' and num2 == 0):
-            while(num2 == 0):
-                print("Error, can not divide by 0")
-                num2 = float(input("Input your second number again"))
-    
-    if(opc == '1'):
-        print(num1 + num2)
-        input()
-    elif(opc == '2'):
-        print(num1 - num2)
-        input()
-    elif(opc == '3'):
-        print(num1 * num2)
-        input()
-    elif(opc == '4'):
-        print(num1 / num2)
-        input()
-    elif(opc == '5'):
-        SciCalc.SciCal()
-        print("over to scientific section")
+                
+            if(opc == '4' and num2 == 0):
+                while(num2 == 0):
+                    print("Error, can not divide by 0")
+                    num2 = float(input("Input your second number again"))
+        
+        if(opc == '1'):
+            print(num1 + num2)
+            input()
+        elif(opc == '2'):
+            print(num1 - num2)
+            input()
+        elif(opc == '3'):
+            print(num1 * num2)
+            input()
+        elif(opc == '4'):
+            print(num1 / num2)
+            input()
+        elif(opc == '5'):
+            SciCalc.SciCal()
+            print("over to scientific section")
 
 
-    if(opc =='x'):
-        input("Press Enter to Exit the Program")
+        if(opc =='x'):
+            input("Press Enter to Exit the Program")
+def main():
+    simpleCalc()
+
+if __name__ == "__main__":
+    simpleCalc()  
